c9d19c137b8a223ec5939b7128387ae7e1bec88a
Andrew Bayly <andrew.bayly@mac.com>
2024-03-11 20:28:13 -0700

    updates

--- app.js
+++ app.js
@@ -2,60 +2,92 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+async function filteredResponses(req, res){ 
+
+  const result = await getSubmissions(req)
+
+  res.type('json').send(result)
+}
+
+async function getSubmissions(req){ 
+
+  const formId = req.params.formId
+
+  const baseURL = 'https://api.fillout.com'
+  const url = `/v1/api/forms/${formId}/submissions`
+
+  const response = await fetch(baseURL + url, {
+
+    headers : { 
+      "Authorization" : "Bearer sk_prod_TfMbARhdgues5AuIosvvdAC9WsA5kXiZlW8HZPaRDlIbCpSpLsXBeZO7dCVZQwHAY3P4VSBPiiC33poZ1tdUj2ljOzdTCCOSpUZ_3912"
+    }
+
+  })
+  
+  const content = await response.json()
+  
+  return content
+}
+
+/**
+async function main(){ 
+  console.log('Hello World')
+  
+  const baseURL = 'https://api.fillout.com'
+  const url = '/v1/api/forms/cLZojxk94ous/submissions'
+  
+  const response = await fetch(baseURL + url, {
+
+    headers : { 
+      "Authorization" : "Bearer sk_prod_TfMbARhdgues5AuIosvvdAC9WsA5kXiZlW8HZPaRDlIbCpSpLsXBeZO7dCVZQwHAY3P4VSBPiiC33poZ1tdUj2ljOzdTCCOSpUZ_3912"
+    }
+
+  })
+  
+  const content = await response.text()
+  
+  console.log(JSON.parse(content))
+    
+}
+
+main()
+**/
+
+
+app.get("/:formId/filteredResponses", filteredResponses);
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      WooHoo!!! from Render!
-    </section>
-  </body>
-</html>
-`
+
+
+
+/**
+ TODO: 
+ ----
+   - print "hello world" to the console on server start-up. - DONE!
+   - access api.fillout.com and print out sample data to the console. - DONE!
+   - implement a test end-point which calls the above method to get the data.
+   - test the end-point by calling the server from a test.js app
+   
+   
+   
+ NOTES:
+ -----  
+   - NOTE: There is no type information on the filter value so if the value is a 
+     string then we have to infer if it should be treated a date or a string. 
+     I chose to do this as follows: 
+       if condition is greater_than or less_than then assume it's a date
+       if condition is equals or does_not_equal then assume it's a string      
+   
+     That's just one approach. The ideal solution is to add the type. Failing that
+     we could infer the type by the formatting of the string - if it looks like 
+     a date then it's a date. However this is messy and involves building a 
+   
+**/
+
+
+
+
